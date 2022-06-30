8177134eaeb8708a1070a8caaa4888c81565b24d
Matteus Isaque <matteus.isaque28@gmail.com>
2022-06-30 15:57:52 -0300

    ok

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -20,7 +20,7 @@ export function setupAPIClient(ctx = undefined) {
     return response;
   }, (error: AxiosError) => {
     if (error.response.status === 401) {
-      if (error.response.data?.code === 'token.expired') {
+      if (error.response.data === 'token.expired') {
         cookies = parseCookies(ctx);
 
         const { 'isaquesestudios.refreshToken': refreshToken } = cookies;
